9b1dc2b1338bf6d4676727c5d12ddfda0ec3610d
StarkasteBamse <sulevi.sihvola@gmail.com>
2018-09-12 13:43:27 +0300

    3.3

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 let persons = [
-    {name: 'Arto Hellas', number: '040-123456', id: 1},
-    {name: 'Matti Tienari', number: '040-123456', id: 2},
-    {name: 'Arto Järvinen', number: '040-123456', id: 3},
-    {name: 'Lea Kutvonen', number: '040-123456', id: 4}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Matti Tienari', number: '040-123456', id: 2 },
+    { name: 'Arto Järvinen', number: '040-123456', id: 3 },
+    { name: 'Lea Kutvonen', number: '040-123456', id: 4 }
 ];
 
 app.get('/api/persons', (req, res) => {
@@ -14,7 +14,17 @@ app.get('/api/persons', (req, res) => {
 
 app.get('/info', (req, res) => {
     res.send('puhelinluettelossa on ' + persons.length + ' henkilön tiedot'
-    + '</br>' + new Date());
+        + '</br>' + new Date());
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).end();
+    };
 });
 
 const PORT = 3001
